5068902617bc0931835c88b45caaf027fc211a74
Shahab nf <shahab.nikfetrat@gmail.com>
2023-04-28 11:32:35 -0400

    First commit create weather.py file

---
+++ weather.py
@@ -0,0 +1,13 @@
+
+
+# city name
+city_name = ""
+
+# Welcome banner
+print("Welcome to weather forcast")
+
+# get the city name
+city_name = input("Please enter the city name: ")
+
+# Check the input
+print(city_name)
